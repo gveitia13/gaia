414eabc1b874d05464f0a4c381104822d2f26436
Gilbert Veitia Urbay <61420914+gveitia13@users.noreply.github.com>
2023-01-30 04:40:04 -0500

    no toy pa esto ya

--- app_main/admin.py
+++ app_main/admin.py
@@ -125,9 +125,90 @@ class MunicipioAdmin(admin.ModelAdmin):
 
 
 class OrdenAdmin(admin.ModelAdmin):
-    list_display = ('status', 'uuid', 'date_created', 'total', 'moneda', 'nombre_comprador', 'municipio')
+    list_display = ('status', 'uuid', 'date_created', 'total', 'moneda', 'correo', 'municipio')
     list_filter = ('status', 'moneda', 'municipio')
     search_fields = ('uuid',)
+    actions = ['Exportar_Excel']
+
+    def Exportar_Excel(self, request, queryset):
+        import datetime
+        from django.http import HttpResponse
+        from openpyxl import Workbook
+        from io import BytesIO
+
+        qs = Orden.objects.all()
+        filas = [i[0] for i in set(queryset.values_list('correo'))]
+        print(filas)
+        columnas = [Product.objects.get(pk=i[0]).name for i in
+                    set(ComponenteOrden.objects.filter(orden__in=queryset).values_list('producto_id'))]
+        filas2 = []
+        for i in filas:
+            a = Orden.objects.filter(correo=i).first()
+            filas2.append(f'{a.nombre_receptor}\n {a.telefono_receptor}\n {a.municipio}')
+        print(columnas)
+
+        # book = openpyxl.Workbook()
+        # sheet = book.active
+        # export = [columnas]
+        #
+        # sheet.append(columnas)
+        #
+        # for ob in object_list:
+        #     sheet.append((
+        #         f'{ob["nombre"]} {ob["apellidos"]}',
+        #         ob["categoria_ocupacional"],
+        #         ob['categoria_cientifica'],
+        #         ob['rrhh']['institucion'],
+        #         ob['rrhh']['clasificador_entidad'],
+        #         int(ob['rrhh']['porciento_de_participacion']),
+        #         ob['rrhh']['salario_mensual'],
+        #         ob['rrhh']['salario_anual_ejecutora'],
+        #         ob['rrhh']['salario_anual_externo'],
+        #         int(ob['rrhh']['porciento_de_remuneracion']),
+        #         ob['rrhh']['mce'],
+        #         ob['rrhh']['tiempo'],
+        #         ob['rrhh']['anual'],
+        #         ob['rrhh']['salario_mensual_basico'],
+        #     ))
+        #
+        # today = datetime.now()
+        # strToday = today.strftime("%Y-%m-%d")
+        #
+        # sheet = excel.pe.Sheet(export)
+        #
+        # return excel.make_response(sheet, "xlsx",
+        #                                file_name=f"Resumen de salarios {project} " + strToday + ".xlsx")
+        #
+        # sheet.title = f'{project}'
+        # response = HttpResponse(content_type='application / msexcel')
+        # response['Content-Disposition'] = f'attachment;filename=Anexo 3 {project} {strToday}.xlsx'
+        # book.save(response)
+        # return response
+        wb = Workbook()  # Genere un libro de trabajo (es decir, un archivo de Excel)
+        wb.encoding = 'utf-8'
+        sheet1 = wb.active  # Obtenga la primera hoja de trabajo (hoja1)
+        sheet1.title = 'Ordenes'
+        sheet1.append([''] + columnas)
+
+        filas1 = [''] + filas2
+        for i in range(1, len(filas1) + 1):
+            sheet1.cell(row=i, column=1).value = filas1[i - 1]
+
+        output = BytesIO()
+        wb.save(output)  # Guarde el contenido del archivo de Excel en IO
+
+        output.seek(0)  # Reposición al principio
+        response = HttpResponse(output.getvalue(), content_type='application/vnd.ms-excel')
+        ctime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        file_name = 'Información registrada % s.xls' % ctime
+        response['Content-Disposition'] = 'attachment; filename=%s' % file_name
+        return response
+        for i in qs:
+            for a in i.componente_orden.all():
+                pass
+        print(ComponenteOrden.objects.filter(orden__in=Orden.objects.all()).count())
+        # for i in ComponenteOrden.objects.filter(orden__in=queryset):
+        #     print(i)
 
 
 class ComponenteOrdenAdmin(admin.ModelAdmin):
